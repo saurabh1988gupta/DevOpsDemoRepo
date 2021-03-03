01c82d0ad15afa5eebf81b0eec7fb992ccfd825d
Saurabh Gupta <saurabh1988gupta@gmail.com>
2021-03-04 02:16:03 +0530

    Initial Commit

---
+++ devops.practice/src/main/java/devops/devops/practice/App.java
@@ -0,0 +1,13 @@
+package devops.devops.practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
